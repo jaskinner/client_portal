169ae08791d76de04bbd0bad3c48b98a7cf2af2e
Jon Skinner <jon.a.skinner1@gmail.com>
2022-01-24 22:56:33 -0500

    auth doesn't work yet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,75 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
+const express = require("express"),
+    morgan = require("morgan"),
+    path = require("path"),
+    cookieParser = require("cookie-parser"),
+    session = require("express-session"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local").Strategy,
+    bodyParser = require("body-parser"),
+    flash = require("express-flash");
 
+const app = express().use(morgan("combined"));
 
-const app = express().use(morgan("dev"));
+const session_configuration = {
+    secret: "something super secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+};
+
+session_configuration.cookie.secure = false;
+
+app.use(flash);
+app.use(session(session_configuration));
+app.use(cookieParser("something super secret"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+const users = {
+    id12345: {
+        id: 12345,
+        username: "jaskinner",
+        password: "bish",
+    },
+    id1: {
+        id: 1,
+        username: "admin",
+        password: "admin123",
+    },
+};
+
+passport.use(
+    new LocalStrategy((username, password, done) => {
+        for (userid in users) {
+            const user = users[userid];
+            if (user.username.toLowerCase() === username.toLowerCase()) {
+                if (user.password === password) {
+                    return done(null, user);
+                }
+            }
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    if (users["id"] + user.id) {
+        done(null, "id" + user.id);
+    } else {
+        done(new Error("CANT_SERIALIZE_INVALID_USER"));
+    }
+});
+
+passport.deserializeUser((userid, done) => {
+    if (users[userid]) {
+        done(null, users[userid]);
+    } else {
+        done(new Error("CANT_FIND_USER_TO_SERIALIZE"));
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,12 +81,17 @@ app.locals.basedir = path.join(__dirname, "/../public");
 app.get("/", (req, res) => res.render("index"));
 
 app.get("/login", (req, res) => {
-  res.render("login", { title: "login" });
+    res.render("login", { title: "login" });
 });
 
 app.post("/login", (req, res) => {
-    
-})
+    passport.authenticate("local", {
+        successRedirect: "/account",
+        failureRedirect: "/login",
+        successFlash: { message: "Welcome back!" },
+        failureFlash: true,
+    });
+});
 
 // generic route not found
 // TODO: replace with better function
