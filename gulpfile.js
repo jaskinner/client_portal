416ad83433af8b636f2d04231f971a61579460e5
Jon Skinner <jon.a.skinner1@gmail.com>
2022-01-25 23:00:26 -0500

    updated gulp for pug copy;

--- gulpfile.js
+++ gulpfile.js
@@ -5,4 +5,5 @@ const eslint = require("gulp-eslint");
 gulp.task("default", () => {
     gulp.src("src/**/*.js").pipe(eslint()).pipe(eslint.format());
     gulp.src("src/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
+    gulp.src("src/**/*.pug").pipe(gulp.dest("dist"))
 });
