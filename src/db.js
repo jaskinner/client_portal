28f373404ecd33b5359877b9c10c466054ce225c
Jon Skinner <jon.a.skinner1@gmail.com>
2022-01-25 23:36:21 -0500

    messing around with db;

--- src/db.js
+++ src/db.js
@@ -3,15 +3,27 @@ const MongoClient = require("mongodb").MongoClient,
     local = require("../local.config.json");
 
 let db;
+const dbName = local.config.db_config.dbName;
 
-exports.init = (cb) => {
-    console.log(`\n** Connecting to db...`);
-    const url = local.config.db_config.host_url;
+exports.init = (callback) => {
+    async.waterfall(
+        [
+            (cb) => {
+                console.log(`\n** Connecting to db...`);
+                const url = local.config.db_config.host_url;
 
-    MongoClient.connect(url, (err, dbase) => {
-        if (err) return cb(err);
-        console.log("** ...Success! Connected to server");
-        db = dbase;
-        cb(null);
-    });
+                MongoClient.connect(url, (err, dbase) => {
+                    if (err) return cb(err);
+                    console.log("** ...Success! Connected to server");
+                    db = dbase;
+                    cb(null);
+                });
+            },
+            (cb) => {
+                db.db(dbName).collection("invoices");
+                cb(null);
+            },
+        ],
+        callback
+    );
 };
